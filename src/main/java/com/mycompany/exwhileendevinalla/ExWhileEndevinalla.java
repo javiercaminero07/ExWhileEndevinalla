a92bbbdd51e39bd0a4fc21b19f6b034623a1c93d
Javier Caminero Rodríguez <25javier.caminero@inscastellet.cat>
2025-11-14 16:40:34 +0100

    Comentari afegit

--- src/main/java/com/mycompany/exwhileendevinalla/ExWhileEndevinalla.java
+++ src/main/java/com/mycompany/exwhileendevinalla/ExWhileEndevinalla.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
+//Has fet un bon treball
 package com.mycompany.exwhileendevinalla;
 
 /**
